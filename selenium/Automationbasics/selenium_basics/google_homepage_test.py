8fa21afa42839568745012b3300640da0cd4fe8f
harshthakur2341 <thakursahab2341@gmail.com>
2026-05-04 09:36:48 +0530

    2/5/26

---
+++ selenium/Automationbasics/selenium_basics/google_homepage_test.py
@@ -0,0 +1,48 @@
+"""
+homepage error
+"""
+from time import sleep
+
+
+from selenium import webdriver
+from selenium.webdriver.edge.service import Service
+
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+
+browser = input('What broswer do you want ? ')
+
+match(browser.lower()):
+    case 'chrome':
+        driver = webdriver.Chrome(service=Service('../resources/chromedriver.exe'))
+
+    case 'edge':
+        driver = webdriver.Edge(service= Service('../resources/msedgedriver.exe'))
+
+    case _:
+        print('unknown broswer - not avaiable ')
+        driver = webdriver.Edge(service=Service('../resources/msedgedriver.exe'))
+
+
+
+
+
+
+
+
+
+
+driver = webdriver.Edge(service=Service('../resources/msedgedriver.exe'))
+
+driver.get("https://www.google.com")
+
+pagetitle = driver.title
+
+if pagetitle == 'Google':
+    print('Pass')
+else:
+    print('Fail')
+
+
+sleep(10)
+driver.quit()
+
